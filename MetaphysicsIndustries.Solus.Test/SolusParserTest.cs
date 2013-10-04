a5b86670d840f62c5c243ca98fd97d00844b13ce
izrik <izrik@izrik.com>
2013-10-04 09:19:54 -0500

    Add more tests for the up-coming new parser.

--- MetaphysicsIndustries.Solus.Test/SolusParserTest.cs
+++ MetaphysicsIndustries.Solus.Test/SolusParserTest.cs
@@ -31,7 +31,7 @@ namespace MetaphysicsIndustries.Solus.Test
             Assert.IsInstanceOf<MultiplicationOperation>(fcall.Function);
             Assert.AreEqual(2, fcall.Arguments.Count);
             Assert.IsInstanceOf<VariableAccess>(fcall.Arguments[0]);
-            Assert.AreEqual("a", (fcall.Arguments[0] as VariableAccess).Variable.Name);
+            Assert.AreEqual("a", (fcall.Arguments[0] as VariableAccess).VariableName);
             Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[1]);
             var fcall2 = (FunctionCall)fcall.Arguments[1];
             Assert.IsInstanceOf<AdditionOperation>(fcall2.Function);
@@ -39,7 +39,7 @@ namespace MetaphysicsIndustries.Solus.Test
             Assert.IsInstanceOf<Literal>(fcall2.Arguments[0]);
             Assert.AreEqual(2.0f, (fcall2.Arguments[0] as Literal).Value);
             Assert.IsInstanceOf<VariableAccess>(fcall2.Arguments[1]);
-            Assert.AreEqual("c", (fcall2.Arguments[1] as VariableAccess).Variable.Name);
+            Assert.AreEqual("c", (fcall2.Arguments[1] as VariableAccess).VariableName);
         }
 
         [Test]
@@ -68,7 +68,7 @@ namespace MetaphysicsIndustries.Solus.Test
                 }
                 else
                 {
-                    varnames.Add((arg as VariableAccess).Variable.Name);
+                    varnames.Add((arg as VariableAccess).VariableName);
                 }
             }
 
@@ -79,9 +79,339 @@ namespace MetaphysicsIndustries.Solus.Test
         }
 
         [Test]
-        public void TestFunctionCalls()
+        public void TestMixedOperandsCommutativeAssociativeOperators1()
         {
             var parser = new SolusParser();
+
+            var expr = parser.Compile("1 + a + 2 * b * 3 * c");
+
+            Assert.IsInstanceOf<FunctionCall>(expr);
+            var fcall = (FunctionCall)expr;
+
+            Assert.AreSame(AdditionOperation.Value, fcall.Function);
+            Assert.AreEqual(3, fcall.Arguments.Count);
+
+            Assert.IsInstanceOf<Literal>(fcall.Arguments[0]);
+            Assert.AreEqual(1.0f, (fcall.Arguments[0] as Literal).Value);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall.Arguments[1]);
+            Assert.AreEqual("a", (fcall.Arguments[1] as VariableAccess).VariableName);
+
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[2]);
+            var fcall2 = (FunctionCall)fcall.Arguments[2];
+            Assert.AreSame(MultiplicationOperation.Value, fcall2.Function);
+            Assert.AreEqual(4, fcall2.Arguments.Count);
+
+            Assert.IsInstanceOf<Literal>(fcall2.Arguments[0]);
+            Assert.AreEqual(2.0f, (fcall2.Arguments[0] as Literal).Value);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall2.Arguments[1]);
+            Assert.AreEqual("b", (fcall2.Arguments[1] as VariableAccess).VariableName);
+
+            Assert.IsInstanceOf<Literal>(fcall2.Arguments[2]);
+            Assert.AreEqual(3.0f, (fcall2.Arguments[2] as Literal).Value);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall2.Arguments[3]);
+            Assert.AreEqual("c", (fcall2.Arguments[3] as VariableAccess).VariableName);
+        }
+
+        [Test]
+        public void TestMixedOperandsCommutativeAssociativeOperators2()
+        {
+            var parser = new SolusParser();
+
+            var expr = parser.Compile("1 + a * 2 * b * 3 + c");
+
+            Assert.IsInstanceOf<FunctionCall>(expr);
+            var fcall = (FunctionCall)expr;
+
+            Assert.AreSame(AdditionOperation.Value, fcall.Function);
+            Assert.AreEqual(3, fcall.Arguments.Count);
+
+            Assert.IsInstanceOf<Literal>(fcall.Arguments[0]);
+            Assert.AreEqual(1.0f, (fcall.Arguments[0] as Literal).Value);
+
+
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[1]);
+            var fcall2 = (FunctionCall)fcall.Arguments[1];
+            Assert.AreSame(MultiplicationOperation.Value, fcall2.Function);
+            Assert.AreEqual(4, fcall2.Arguments.Count);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall2.Arguments[0]);
+            Assert.AreEqual("a", (fcall2.Arguments[0] as VariableAccess).VariableName);
+
+            Assert.IsInstanceOf<Literal>(fcall2.Arguments[1]);
+            Assert.AreEqual(2.0f, (fcall2.Arguments[1] as Literal).Value);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall2.Arguments[2]);
+            Assert.AreEqual("b", (fcall2.Arguments[2] as VariableAccess).VariableName);
+
+            Assert.IsInstanceOf<Literal>(fcall2.Arguments[3]);
+            Assert.AreEqual(3.0f, (fcall2.Arguments[3] as Literal).Value);
+
+
+            Assert.IsInstanceOf<VariableAccess>(fcall.Arguments[2]);
+            Assert.AreEqual("c", (fcall.Arguments[2] as VariableAccess).VariableName);
+        }
+
+        [Test]
+        public void TestMixedOperandsCommutativeAssociativeOperators3()
+        {
+            var parser = new SolusParser();
+
+            var expr = parser.Compile("1 * a * 2 * b + 3 + c");
+
+            Assert.IsInstanceOf<FunctionCall>(expr);
+            var fcall = (FunctionCall)expr;
+            Assert.AreSame(AdditionOperation.Value, fcall.Function);
+            Assert.AreEqual(3, fcall.Arguments.Count);
+
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[0]);
+            var fcall2 = (FunctionCall)fcall.Arguments[0];
+            Assert.AreSame(MultiplicationOperation.Value, fcall2.Function);
+            Assert.AreEqual(4, fcall2.Arguments.Count);
+
+            Assert.IsInstanceOf<Literal>(fcall2.Arguments[0]);
+            Assert.AreEqual(1.0f, (fcall2.Arguments[0] as Literal).Value);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall2.Arguments[1]);
+            Assert.AreEqual("a", (fcall2.Arguments[1] as VariableAccess).VariableName);
+
+            Assert.IsInstanceOf<Literal>(fcall2.Arguments[2]);
+            Assert.AreEqual(2.0f, (fcall2.Arguments[2] as Literal).Value);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall2.Arguments[3]);
+            Assert.AreEqual("b", (fcall2.Arguments[3] as VariableAccess).VariableName);
+
+            Assert.IsInstanceOf<Literal>(fcall.Arguments[1]);
+            Assert.AreEqual(3.0f, (fcall.Arguments[1] as Literal).Value);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall.Arguments[2]);
+            Assert.AreEqual("c", (fcall.Arguments[2] as VariableAccess).VariableName);
+        }
+
+        [Test]
+        public void TestMixedOperandsCommutativeAssociativeOperators4()
+        {
+            var parser = new SolusParser();
+
+            var expr = parser.Compile("1 * a * 2 + b + 3 * c");
+
+            Assert.IsInstanceOf<FunctionCall>(expr);
+            var fcall = (FunctionCall)expr;
+            Assert.AreSame(AdditionOperation.Value, fcall.Function);
+            Assert.AreEqual(3, fcall.Arguments.Count);
+
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[0]);
+            var fcall2 = (FunctionCall)fcall.Arguments[0];
+            Assert.AreSame(MultiplicationOperation.Value, fcall2.Function);
+            Assert.AreEqual(3, fcall2.Arguments.Count);
+
+            Assert.IsInstanceOf<Literal>(fcall2.Arguments[0]);
+            Assert.AreEqual(1.0f, (fcall2.Arguments[0] as Literal).Value);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall2.Arguments[1]);
+            Assert.AreEqual("a", (fcall2.Arguments[1] as VariableAccess).VariableName);
+
+            Assert.IsInstanceOf<Literal>(fcall2.Arguments[2]);
+            Assert.AreEqual(2.0f, (fcall2.Arguments[2] as Literal).Value);
+
+
+
+            Assert.IsInstanceOf<VariableAccess>(fcall.Arguments[1]);
+            Assert.AreEqual("b", (fcall.Arguments[1] as VariableAccess).VariableName);
+
+
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[2]);
+            fcall2 = (FunctionCall)fcall.Arguments[2];
+            Assert.AreSame(MultiplicationOperation.Value, fcall2.Function);
+            Assert.AreEqual(2, fcall2.Arguments.Count);
+
+            Assert.IsInstanceOf<Literal>(fcall2.Arguments[0]);
+            Assert.AreEqual(3.0f, (fcall2.Arguments[0] as Literal).Value);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall2.Arguments[1]);
+            Assert.AreEqual("c", (fcall2.Arguments[1] as VariableAccess).VariableName);
+        }
+
+        [Test]
+        public void TestMixedOperandsCommutativeAssociativeOperators5()
+        {
+            var parser = new SolusParser();
+
+            var expr = parser.Compile("1 * a + 2 + b * 3 * c");
+
+            Assert.IsInstanceOf<FunctionCall>(expr);
+            var fcall = (FunctionCall)expr;
+            Assert.AreSame(AdditionOperation.Value, fcall.Function);
+            Assert.AreEqual(3, fcall.Arguments.Count);
+
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[0]);
+            var fcall2 = (FunctionCall)fcall.Arguments[0];
+            Assert.AreSame(MultiplicationOperation.Value, fcall2.Function);
+            Assert.AreEqual(2, fcall2.Arguments.Count);
+
+            Assert.IsInstanceOf<Literal>(fcall2.Arguments[0]);
+            Assert.AreEqual(1.0f, (fcall2.Arguments[0] as Literal).Value);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall2.Arguments[1]);
+            Assert.AreEqual("a", (fcall2.Arguments[1] as VariableAccess).VariableName);
+
+
+            Assert.IsInstanceOf<Literal>(fcall.Arguments[1]);
+            Assert.AreEqual(2.0f, (fcall.Arguments[1] as Literal).Value);
+
+
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[2]);
+            fcall2 = (FunctionCall)fcall.Arguments[2];
+            Assert.AreSame(MultiplicationOperation.Value, fcall2.Function);
+            Assert.AreEqual(3, fcall2.Arguments.Count);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall2.Arguments[0]);
+            Assert.AreEqual("b", (fcall2.Arguments[0] as VariableAccess).VariableName);
+
+            Assert.IsInstanceOf<Literal>(fcall2.Arguments[1]);
+            Assert.AreEqual(3.0f, (fcall2.Arguments[1] as Literal).Value);
+
+            Assert.IsInstanceOf<VariableAccess>(fcall2.Arguments[2]);
+            Assert.AreEqual("c", (fcall2.Arguments[2] as VariableAccess).VariableName);
+        }
+
+        [Test]
+        public void TestMixedOperandsAscendingPrecedence()
+        {
+            var parser = new SolusParser();
+
+            var expr = parser.Compile("1 | a & 2 + b * 3 ^ c");
+
+            Assert.IsInstanceOf<FunctionCall>(expr);
+            var fcall = (FunctionCall)expr;
+            Assert.AreSame(BitwiseOrOperation.Value, fcall.Function);
+            Assert.AreEqual(2, fcall.Arguments.Count);
+            Assert.IsInstanceOf<Literal>(fcall.Arguments[0]);
+            Assert.AreEqual(1.0f, (fcall.Arguments[0] as Literal).Value);
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[1]);
+
+            fcall = (FunctionCall)(fcall.Arguments[1]);
+            Assert.AreSame(BitwiseAndOperation.Value, fcall.Function);
+            Assert.AreEqual(2, fcall.Arguments.Count);
+            Assert.IsInstanceOf<VariableAccess>(fcall.Arguments[0]);
+            Assert.AreEqual("a", (fcall.Arguments[0] as VariableAccess).VariableName);
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[1]);
+
+            fcall = (FunctionCall)(fcall.Arguments[1]);
+            Assert.AreSame(AdditionOperation.Value, fcall.Function);
+            Assert.AreEqual(2, fcall.Arguments.Count);
+            Assert.IsInstanceOf<Literal>(fcall.Arguments[0]);
+            Assert.AreEqual(2.0f, (fcall.Arguments[0] as Literal).Value);
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[1]);
+
+            fcall = (FunctionCall)(fcall.Arguments[1]);
+            Assert.AreSame(MultiplicationOperation.Value, fcall.Function);
+            Assert.AreEqual(2, fcall.Arguments.Count);
+            Assert.IsInstanceOf<VariableAccess>(fcall.Arguments[0]);
+            Assert.AreEqual("b", (fcall.Arguments[0] as VariableAccess).VariableName);
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[1]);
+
+            fcall = (FunctionCall)(fcall.Arguments[1]);
+            Assert.AreSame(ExponentOperation.Value, fcall.Function);
+            Assert.AreEqual(2, fcall.Arguments.Count);
+            Assert.IsInstanceOf<Literal>(fcall.Arguments[0]);
+            Assert.AreEqual(3.0f, (fcall.Arguments[0] as Literal).Value);
+            Assert.IsInstanceOf<VariableAccess>(fcall.Arguments[1]);
+            Assert.AreEqual("c", (fcall.Arguments[1] as VariableAccess).VariableName);
+
+        }
+
+        [Test]
+        public void TestMixedOperandsDescendingPrecedence()
+        {
+            var parser = new SolusParser();
+
+            var expr = parser.Compile("1 ^ a * 2 + b & 3 | c");
+
+            Assert.IsInstanceOf<FunctionCall>(expr);
+            var fcall = (expr as FunctionCall);
+            Assert.AreSame(BitwiseOrOperation.Value, fcall.Function);
+            Assert.AreEqual(2, fcall.Arguments.Count);
+            Assert.IsInstanceOf<VariableAccess>(fcall.Arguments[1]);
+            Assert.AreEqual("c", (fcall.Arguments[1] as VariableAccess).VariableName);
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[0]);
+
+            fcall = (fcall.Arguments[0] as FunctionCall);
+            Assert.AreSame(BitwiseAndOperation.Value, fcall.Function);
+            Assert.AreEqual(2, fcall.Arguments.Count);
+            Assert.IsInstanceOf<Literal>(fcall.Arguments[1]);
+            Assert.AreEqual(3.0f, (fcall.Arguments[1] as Literal).Value);
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[0]);
+
+            fcall = (fcall.Arguments[0] as FunctionCall);
+            Assert.AreSame(AdditionOperation.Value, fcall.Function);
+            Assert.AreEqual(2, fcall.Arguments.Count);
+            Assert.IsInstanceOf<VariableAccess>(fcall.Arguments[1]);
+            Assert.AreEqual("b", (fcall.Arguments[1] as VariableAccess).VariableName);
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[0]);
+
+            fcall = (fcall.Arguments[0] as FunctionCall);
+            Assert.AreSame(MultiplicationOperation.Value, fcall.Function);
+            Assert.AreEqual(2, fcall.Arguments.Count);
+            Assert.IsInstanceOf<Literal>(fcall.Arguments[1]);
+            Assert.AreEqual(2.0f, (fcall.Arguments[1] as Literal).Value);
+            Assert.IsInstanceOf<FunctionCall>(fcall.Arguments[0]);
+
+            fcall = (fcall.Arguments[0] as FunctionCall);
+            Assert.AreSame(ExponentOperation.Value, fcall.Function);
+            Assert.AreEqual(2, fcall.Arguments.Count);
+            Assert.IsInstanceOf<VariableAccess>(fcall.Arguments[1]);
+            Assert.AreEqual("a", (fcall.Arguments[1] as VariableAccess).VariableName);
+            Assert.IsInstanceOf<Literal>(fcall.Arguments[0]);
+            Assert.AreEqual(1.0f, (fcall.Arguments[0] as Literal).Value);
+        }
+
+        [Test]
+        public void TestMixedOperands()
+        {
+            var parser = new SolusParser();
+
+            var expr = parser.Compile("1 + a * 2 + b * 3 * c");
+
+            Assert.IsInstanceOf<FunctionCall>(expr);
+            var fcall = (FunctionCall)expr;
+
+            Assert.IsInstanceOf<AdditionOperation>(fcall.Function);
+            Assert.GreaterOrEqual(fcall.Arguments.Count, 4);
+            Assert.LessOrEqual(fcall.Arguments.Count, 6);
+
+            Set<string> varnames = new Set<string>();
+
+            float sum = 0;
+            foreach (var arg in fcall.Arguments)
+            {
+                Assert.That(arg is Literal || arg is VariableAccess);
+                if (arg is Literal)
+                {
+                    sum += (arg as Literal).Value;
+                }
+                else
+                {
+                    varnames.Add((arg as VariableAccess).VariableName);
+                }
+            }
+
+            Assert.AreEqual(6.0f, sum);
+            Assert.That(varnames.Contains("a"));
+            Assert.That(varnames.Contains("b"));
+            Assert.That(varnames.Contains("c"));
+        }
+
+        [Test]
+        public void TestFunctionCall1()
+        {
+            var parser = new SolusParser();
+            VariableTable vars = new VariableTable();
+            Variable pi = new Variable("pi");
+            vars.Add("pi", pi);
+            vars.Add(pi, new Literal((float)Math.PI));
 
             var expr = parser.Compile("sin(pi)", cleanup:false);
 
@@ -89,21 +419,31 @@ namespace MetaphysicsIndustries.Solus.Test
             var fcall = (FunctionCall)expr;
             Assert.IsInstanceOf<SineFunction>(fcall.Function);
             Assert.AreEqual(1, fcall.Arguments.Count);
-            Assert.IsInstanceOf<Literal>(fcall.Arguments[0]);
-            Assert.AreEqual(Math.PI, (fcall.Arguments[0] as Literal).Value, 0.0001f);
-            Assert.AreEqual(0f, fcall.Eval(new VariableTable()).Value, 0.0001f);
+//            Assert.IsInstanceOf<Literal>(fcall.Arguments[0]);
+//            Assert.AreEqual(Math.PI, (fcall.Arguments[0] as Literal).Value, 0.0001f);
+            Assert.AreEqual(0f, fcall.Eval(vars).Value, 0.0001f);
+        }
 
+        [Test]
+        public void TestFunctionCall2()
+        {
+            var parser = new SolusParser();
 
-            expr = parser.Compile("derive(x, x)", cleanup:false);
+            var expr = parser.Compile("derive(x, x)", cleanup:false);
 
             Assert.IsInstanceOf<Literal>(expr);
             Assert.AreEqual(1f, (expr as Literal).Value);
+        }
 
+        [Test]
+        public void TestFunctionCall3()
+        {
+            var parser = new SolusParser();
 
-            expr = parser.Compile("if(1, 2, 3)", cleanup:false);
+            var expr = parser.Compile("if(1, 2, 3)", cleanup:false);
 
             Assert.IsInstanceOf<FunctionCall>(expr);
-            fcall = (FunctionCall)expr;
+            var fcall = (FunctionCall)expr;
             Assert.IsInstanceOf<IfFunction>(fcall.Function);;
             Assert.AreEqual(3, fcall.Arguments.Count);
             Assert.IsInstanceOf<Literal>(fcall.Arguments[0]);
