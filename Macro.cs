ea2d1bcdcd1346c831e67b3428a6255de45adb0b
izrik <izrik@izrik.com>
2013-10-10 21:55:09 -0500

    Add the Macro class, to replace ParseFunction.

---
+++ Macro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetaphysicsIndustries.Solus
+{
+    public abstract class Macro
+    {
+        public string Name;
+        public int NumArguments;
+        public bool HasVariableNumArgs;
+
+        public abstract Expression InternalCall(IEnumerable<Expression> args, Dictionary<string, Expression> vars);
+
+        public virtual Expression Call(IEnumerable<Expression> args, Dictionary<string, Expression> vars)
+        {
+            List<Expression> arglist = new List<Expression>(args);
+            if (!HasVariableNumArgs &&
+                arglist.Count != NumArguments)
+            {
+                throw new ArgumentException("Incorrect number of arguments.", "arg");
+            }
+
+            return InternalCall(args, vars);
+        }
+    }
+}
