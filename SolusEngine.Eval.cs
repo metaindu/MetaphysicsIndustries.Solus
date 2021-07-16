a5eb0fa6f2668878555eb081367007ac0875004c
izrik <izrik@izrik.com>
2021-07-16 11:39:26 -0500

    Normalize copyright year.

--- SolusEngine.Eval.cs
+++ SolusEngine.Eval.cs
@@ -1,7 +1,7 @@
 
 /*
  *  MetaphysicsIndustries.Solus
- *  Copyright (C) 2010-2021 Metaphysics Industries, Inc., Richard Sartor
+ *  Copyright (C) 2006-2021 Metaphysics Industries, Inc., Richard Sartor
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
