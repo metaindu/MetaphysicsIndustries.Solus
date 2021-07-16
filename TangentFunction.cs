41a33317448e554ff997527e35311c3f869a468c
izrik <izrik@izrik.com>
2021-07-16 11:20:29 -0500

    Add license notices to all relevant files.

--- TangentFunction.cs
+++ TangentFunction.cs
@@ -1,13 +1,28 @@
 
+/*
+ *  MetaphysicsIndustries.Solus
+ *  Copyright (C) 2006-2021 Metaphysics Industries, Inc., Richard Sartor
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *  USA
+ *
+ */
+
 /*****************************************************************************
  *                                                                           *
  *  TangentFunction.cs                                                       *
- *  24 September 2006                                                        *
- *  Project: Solus, Ligra                                                    *
- *  Written by: Richard Sartor                                               *
- *  Copyright (c) 2006-2021 Metaphysics Industries, Inc.                     *
- *                                                                           *
- *  Converted from C++ to C# on 29 October 2007                              *
  *                                                                           *
  *  The class for the built-in Tangent function.                             *
  *                                                                           *
