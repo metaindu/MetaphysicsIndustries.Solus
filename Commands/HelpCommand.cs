b31bf2264f16a66895665c4d305760f784d1151a
izrik <izrik@izrik.com>
2021-07-17 15:10:59 -0500

    Print out the list of commands in 'help list'. Reformat the output so it looks alittle cleaner.

--- Commands/HelpCommand.cs
+++ Commands/HelpCommand.cs
@@ -7,6 +7,8 @@ namespace MetaphysicsIndustries.Solus.Commands
 {
     public class HelpCommand : Command
     {
+        public static readonly HelpCommand Value = new HelpCommand(null);
+
         private static Dictionary<string, string> _helpLookups = 
             new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
 
@@ -40,13 +42,22 @@ List the available topics:
             if (string.IsNullOrEmpty(topic))
                 topic = "help";
             var text = ConstructText(env, topic);
-            Console.WriteLine(text);
+            Console.Write(text);
+            if (!text.EndsWith("\n"))
+                Console.WriteLine();
         }
         
         public string ConstructText(SolusEnvironment env, string topic = "help")
         {
             if (topic == "help")
                 return DocString;
+
+            if (env.Commands.ContainsKey(topic))
+            {
+                if (!string.IsNullOrEmpty(env.Commands[topic].DocString))
+                    return env.Commands[topic].DocString;
+                return "This command does not provide any information.";
+            }
 
             if (env.Functions.ContainsKey(topic))
             {
@@ -75,6 +86,7 @@ List the available topics:
         {
             var sb = new StringBuilder();
             var line = "";
+            var newline = false;
 
             void AddItem(string item)
             {
@@ -88,6 +100,22 @@ List the available topics:
                 line += item;
             }
 
+            if (env.Commands.Count > 0)
+            {
+                sb.AppendLine("Commands:");
+                line = "";
+                var commands = env.Commands.Keys.ToList();
+                commands.Sort();
+                foreach (var c in commands)
+                    AddItem(c);
+                if (line.Length > 0)
+                    sb.AppendLine("  " + line);
+                newline = true;
+            }
+
+            if (newline) sb.AppendLine();
+            newline = false;
+
             if (env.Functions.Count > 0)
             {
                 sb.AppendLine("Functions:");
@@ -98,8 +126,11 @@ List the available topics:
                     AddItem(f);
                 if (line.Length > 0)
                     sb.AppendLine("  " + line);
-                sb.AppendLine();
+                newline = true;
             }
+
+            if (newline) sb.AppendLine();
+            newline = false;
 
             if (env.Macros.Count > 0)
             {
@@ -111,8 +142,11 @@ List the available topics:
                     AddItem(m);
                 if (line.Length > 0)
                     sb.AppendLine("  " + line);
-                sb.AppendLine();
+                newline = true;
             }
+
+            if (newline) sb.AppendLine();
+            newline = false;
 
             if (env.Variables.Count > 0)
             {
@@ -124,8 +158,11 @@ List the available topics:
                     AddItem(v);
                 if (line.Length > 0)
                     sb.AppendLine("  " + line);
-                sb.AppendLine();
+                newline = true;
             }
+
+            if (newline) sb.AppendLine();
+            newline = false;
 
             if (_helpLookups.Count > 0)
             {
