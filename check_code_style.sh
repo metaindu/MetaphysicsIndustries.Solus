4af99705200bf8ff94a6681f61a07babdb44398a
izrik <izrik@izrik.com>
2021-10-19 23:15:41 -0500

    Check for missing license notices.

--- check_code_style.sh
+++ check_code_style.sh
@@ -6,3 +6,8 @@ grep -n '.......................................................................
 # TODO: check for trailing whitespace on lines
 # TODO: check for single newline at end of file
 # TODO: count TODO's
+
+echo ""
+echo "Files missing license notices:"
+grep -L 'GNU Lesser General Public' \
+    $(find . -name \*.cs -o -name \*.sh -o -name \*.giza)
