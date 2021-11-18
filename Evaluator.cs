78f494821c9fcdda3f645cad9defe9854df61a56
izrik <izrik@izrik.com>
2021-11-18 13:52:19 -0600

    Use the evaluator's own Simplify method.

--- Evaluator.cs
+++ Evaluator.cs
@@ -48,7 +48,7 @@ namespace MetaphysicsIndustries.Solus
 
             var env2 = env.CreateChildEnvironment();
             env2.RemoveVariable(interval.Variable);
-            var expr2 = expr.Simplify(env2);
+            var expr2 = Simplify(expr, env2);
 
             var literal = new Literal(0);
             env2.SetVariable(interval.Variable, literal);
@@ -77,7 +77,7 @@ namespace MetaphysicsIndustries.Solus
             var env2 = env.CreateChildEnvironment();
             env2.RemoveVariable(interval1.Variable);
             env2.RemoveVariable(interval2.Variable);
-            var expr2 = expr.Simplify(env2);
+            var expr2 = Simplify(expr, env2);
 
             var inputs1 = new IMathObject[numSteps1];
             var inputs2 = new IMathObject[numSteps2];
@@ -129,7 +129,7 @@ namespace MetaphysicsIndustries.Solus
             env2.RemoveVariable(interval1.Variable);
             env2.RemoveVariable(interval2.Variable);
             env2.RemoveVariable(interval3.Variable);
-            var expr2 = expr.Simplify(env2);
+            var expr2 = Simplify(expr, env2);
 
             var inputs1 = new IMathObject[numSteps1];
             var inputs2 = new IMathObject[numSteps2];
