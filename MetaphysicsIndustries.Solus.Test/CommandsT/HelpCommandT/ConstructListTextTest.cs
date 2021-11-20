51946b7af33b858a9849d87126ffbad21ca86090
izrik <izrik@izrik.com>
2021-11-20 15:27:41 -0600

    Test the 'help list' output with a custom function.

--- MetaphysicsIndustries.Solus.Test/CommandsT/HelpCommandT/ConstructListTextTest.cs
+++ MetaphysicsIndustries.Solus.Test/CommandsT/HelpCommandT/ConstructListTextTest.cs
@@ -1,4 +1,5 @@
 using MetaphysicsIndustries.Solus.Commands;
+using MetaphysicsIndustries.Solus.Values;
 using NUnit.Framework;
 
 namespace MetaphysicsIndustries.Solus.Test.CommandsT.HelpCommandT
@@ -26,6 +27,38 @@ namespace MetaphysicsIndustries.Solus.Test.CommandsT.HelpCommandT
 Functions:
   abs acos acot acsc asec asin atan atan2 ceil cos cot csc dist distsq floor 
   ln load_image log log10 log2 sec sin size tan unitstep 
+
+Macros:
+  assign delete derive feedback if rand sqrt subst 
+
+Additional topics:
+  solus t 
+", result);
+        }
+
+        [Test]
+        public void WithCustomFunctionYieldsText()
+        {
+            // given
+            var env = new SolusEnvironment();
+            var f = new MockFunction(new[] { Types.Scalar }, "f");
+            f.DocStringV = "asdf";
+            env.AddFunction(f);
+            var cs = new CommandSet();
+            cs.AddCommand(DeleteCommand.Value);
+            cs.AddCommand(FuncAssignCommand.Value);
+            cs.AddCommand(HelpCommand.Value);
+            cs.AddCommand(VarAssignCommand.Value);
+            cs.AddCommand(VarsCommand.Value);
+            // when
+            var result = HelpCommand.ConstructListText(env, cs);
+            // then
+            Assert.AreEqual(@"Commands:
+  delete func_assign help var_assign vars 
+
+Functions:
+  abs acos acot acsc asec asin atan atan2 ceil cos cot csc dist distsq f 
+  floor ln load_image log log10 log2 sec sin size tan unitstep 
 
 Macros:
   assign delete derive feedback if rand sqrt subst 
