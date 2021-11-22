9d096672c319a3d6a9745a5559aeab0e12100413
izrik <izrik@izrik.com>
2021-11-22 09:52:57 -0600

    Clean up.

--- SolusEnvironment.cs
+++ SolusEnvironment.cs
@@ -22,7 +22,6 @@
 
 using System.Collections.Generic;
 using System.Linq;
-using MetaphysicsIndustries.Solus.Commands;
 using MetaphysicsIndustries.Solus.Functions;
 using MetaphysicsIndustries.Solus.Macros;
 
