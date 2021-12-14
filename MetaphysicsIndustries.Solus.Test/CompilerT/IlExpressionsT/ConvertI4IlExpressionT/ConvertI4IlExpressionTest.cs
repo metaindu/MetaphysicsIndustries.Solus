f715bda6c47eda08eaeba6d719042e23b514ae05
izrik <izrik@izrik.com>
2021-12-14 09:33:40 -0600

    typo

--- MetaphysicsIndustries.Solus.Test/CompilerT/IlExpressionsT/ConvertI4IlExpressionT/ConvertI4IlExpressionTest.cs
+++ MetaphysicsIndustries.Solus.Test/CompilerT/IlExpressionsT/ConvertI4IlExpressionT/ConvertI4IlExpressionTest.cs
@@ -43,7 +43,7 @@ namespace MetaphysicsIndustries.Solus.Test.CompilerT.IlExpressionsT.ConvertI4IlE
         }
 
         [Test]
-        public void ConstructorNullArgumenttThrows()
+        public void ConstructorNullArgumentThrows()
         {
             // expect
             var ex = Assert.Throws<ArgumentNullException>(
