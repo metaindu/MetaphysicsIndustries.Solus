c80fd1f81c141a8e3206fab8a7e5764f4527b0f8
izrik <izrik@izrik.com>
2021-12-16 13:18:09 -0600

    Add pop, nop, and some branching instructions.

---
+++ MetaphysicsIndustries.Solus.Test/CompilerT/IlExpressionsT/BrFalseIlExpressionT/BrFalseIlExpressionTest.cs
@@ -0,0 +1,58 @@
+
+/*
+ *  MetaphysicsIndustries.Solus
+ *  Copyright (C) 2006-2021 Metaphysics Industries, Inc., Richard Sartor
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *  USA
+ *
+ */
+
+using System;
+using MetaphysicsIndustries.Solus.Compiler.IlExpressions;
+using NUnit.Framework;
+
+namespace MetaphysicsIndustries.Solus.Test.CompilerT.IlExpressionsT.
+    BrFalseIlExpressionT
+{
+    [TestFixture]
+    public class BrFalseIlExpressionTest
+    {
+        [Test]
+        public void ConstructorCreatesInstance()
+        {
+            // given
+            var target = new MockIlExpression();
+            // when
+            var result = new BrFalseIlExpression(target);
+            // then
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<BrFalseIlExpression>(result);
+            Assert.AreSame(target, result.Target);
+        }
+
+        [Test]
+        public void ConstructorNullTargetThrows()
+        {
+            // expect
+            var ex = Assert.Throws<ArgumentNullException>(
+                () => new BrFalseIlExpression(null));
+            // and
+            Assert.AreEqual(
+                "Value cannot be null.\nParameter name: target",
+                ex.Message);
+        }
+    }
+}
