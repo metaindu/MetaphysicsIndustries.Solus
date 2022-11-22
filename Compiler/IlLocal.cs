fd25b69d51adb9afc60700d958addd948670696e
izrik <izrik@izrik.com>
2022-11-22 10:15:42 -0600

    Delete the local usage pieces.

--- Compiler/IlLocal.cs
+++ Compiler/IlLocal.cs
@@ -26,7 +26,6 @@ namespace MetaphysicsIndustries.Solus.Compiler
 {
     public class IlLocal
     {
-        public IlLocalUsage Usage;
         public string VariableName;
         public Type LocalType;
     }
